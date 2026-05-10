9c400db13bba033ff0e826b492f1412f72b0ae2a
dennisanchez444-boop <dennisanchez444@gmail.com>
2026-05-09 20:16:12 -0500

    Clase unitaria lista

---
+++ src/test/java/com/kakredev/examen/TestVendedorJUnit.java
@@ -0,0 +1,19 @@
+package com.kakredev.examen;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class TestVendedorJUnit {
+
+	@Test
+	public void testCalcularSueldo() {
+
+		Vendedor vendedor = new Vendedor("1723456789");
+		vendedor.setSueldoFijo(500.0);
+
+		double sueldo = vendedor.calcularSueldo();
+
+		assertEquals(500.0, sueldo);
+	}
+}
